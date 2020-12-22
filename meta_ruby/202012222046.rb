1580f775266d0e123f34dc11f7f64e6a36ae3752
jun0222 <nextseason6@gmail.com>
2020-12-22 20:55:04 +0900

    --no-edit

---
+++ meta_ruby/202012222046.rb
@@ -0,0 +1,15 @@
+v1 = 1
+class MyClass
+	v2 = 2
+	local_variables
+	def my_method
+		v3 = 3
+	p	local_variables
+	end
+	p local_variables
+end
+
+p obj = MyClass.new
+p obj.my_method
+p obj.my_method
+p local_variables
